850a96e75318dea18d23a29c507a938a92cffaeb
Amanda Margaretha <amandamargaretha272011@gmail.com>
2023-08-08 14:37:49 +0700

    header pesan

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,6 @@ class Header extends Component {
     if (!this.props.data) return null;
 
     const project = this.props.data.project;
-    const github = this.props.data.github;
     const name = this.props.data.name;
     const description = this.props.data.description;
 
@@ -68,10 +67,7 @@ class Header extends Component {
             <Fade bottom duration={2000}>
               <ul className="social">
                 <a href={project} className="button btn project-btn">
-                  <i className="fa fa-book"></i>Project
-                </a>
-                <a href={github} className="button btn github-btn">
-                  <i className="fa fa-github"></i>Github
+                  <i className="fa fa-shopping-cart"></i>Pesan
                 </a>
               </ul>
             </Fade>
